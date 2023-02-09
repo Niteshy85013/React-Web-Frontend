3938ac205071d85c1e50ecc71bac10faead90999
Niteshy85013 <niteshyadav85013@gmail.com>
2023-02-09 14:27:06 +0545

    final

---
+++ src/Components/Products/Productdetail.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// const Productdetail = () => {
+//   return (
+//     <section id='product-detail'>
+
+//         <div className="contain">
+//             <div className="row">
+//                 <div className="col-md-6">
+//                     <img src='./images/'alt="product-image"/>
+
+//                 </div>
+//                 <div className="col-md-6">
+
+//                 </div>
+//             </div>
+//         </div>
+//     </section>
+//   )
+// }
+
+// export default Productdetail
